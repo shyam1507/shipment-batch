6e613ea3cdfba26419568e25a52c77c705bcb6a3
Shyam1997 <ershyam09@gmail.com>
2025-07-14 18:42:05 +0530

    Initial commit of shipment-batch project

---
+++ src/main/java/com/spg/batch/mapper/ShipmentMapper.java
@@ -0,0 +1,10 @@
+package com.spg.batch.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.spg.batch.model.Shipment;
+
+@Mapper
+public interface ShipmentMapper {
+    void insertShipment(Shipment shipment);
+}
---
+++ src/main/java/com/spg/batch/model/Shipment.java
@@ -0,0 +1,12 @@
+package com.spg.batch.model;
+
+import lombok.Data;
+
+@Data
+public class Shipment {
+	private Long id;
+	private String orderId;
+	private String customerName;
+	private String address;
+	private String status;
+}
