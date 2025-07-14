be22952481b9bd50303d4ec30435c62e7622e58e
Shyam1997 <ershyam09@gmail.com>
2025-07-14 18:30:45 +0530

    Initial commit of shipment-batch project

---
+++ src/main/java/com/spg/batch/ShipmentBatchApplication.java
@@ -0,0 +1,13 @@
+package com.spg.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spg/batch/ShipmentBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spg.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
